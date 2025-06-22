d7d4a1755883a851d6c4db45b53a851dc0e70aa4
Joydip Das <190102.cse@student.just.edu.bd>
2025-06-22 11:19:48 +0600

    auth added

--- master/models.py
+++ master/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+class Company(models.Model):
+    company_name = models.CharField(max_length=255)
+    incharge_name = models.CharField(max_length=255)
+    phone_no = models.CharField(max_length=20)
+    email = models.EmailField()
+    address = models.TextField()
+    country = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.company_name
--- master/urls.py
+++ master/urls.py
@@ -1,6 +1,10 @@
-from django.urls import path
-from .views import *
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from .views import CompanyViewSet
 
-urlpatterns =[
-    
+router = DefaultRouter()
+router.register(r'companies', CompanyViewSet)
+
+urlpatterns = [
+    path('', include(router.urls)),
 ]
