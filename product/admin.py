c97acdd6ccf8ca0c4a15c8fb5dd52c090c0670e5
fahimfaisal1454 <fahimfaisal1454@gmail.com>
2025-11-01 15:41:34 +0600

    admin.py

--- product/admin.py
+++ product/admin.py
@@ -1,7 +1,6 @@
 from django.contrib import admin
 from .models import *
 
-
 admin.site.register(ProductCategory)
 admin.site.register(Product)
 admin.site.register(PurchaseProduct)
@@ -10,7 +9,6 @@ admin.site.register(SupplierPurchase)
 admin.site.register(StockProduct)
 admin.site.register(SupplierPurchaseReturn)
 
-
-
-
-
+# Orders (minimal, same style)
+admin.site.register(Order)
+admin.site.register(OrderItem)
